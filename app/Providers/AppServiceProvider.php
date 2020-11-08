bd69dd4916cca6e67a81e52f619f6526d52c5a20
holiq <holiq.ibrahim376@gmail.com>
2020-11-08 21:46:07 +0700

    setup inertia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Inertia::share('auth.user', function (){
+            return[
+                'loggedIn' => Auth::check(),
+            ];
+        });
     }
 }
